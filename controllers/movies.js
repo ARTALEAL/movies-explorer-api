9ee5208e947c947d2c7910776070f52f95963798
ARTALEAL <artaleal@yandex.ru>
2023-04-11 17:16:55 +0300

    movies add/get

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,3 +2,54 @@ const Movie = require('../models/movie');
 const { BadRequestError } = require('../errors/BadRequestError');
 const { NotFoundError } = require('../errors/NotFoundError');
 const { ForbiddenError } = require('../errors/ForbiddenError');
+
+function getMovies(req, res, next) {
+  const owner = req.user._id;
+  Movie
+    .find({ owner })
+    .then((movies) => {
+      if (!movies || movies.length === 0) {
+        res.send('Сохраненных фильмов не найдено');
+      }
+      return res.send(movies);
+    })
+    .catch(next);
+}
+
+function addMovie(req, res, next) {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: req.user._id,
+  })
+    .then((newMovie) => res.send(newMovie))
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        next(new BadRequestError('Неверные данные'));
+      } else {
+        next(err);
+      }
+    });
+}
