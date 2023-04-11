6a168357e152367506e1af520e087e4944311a63
ARTALEAL <artaleal@yandex.ru>
2023-04-11 21:58:07 +0300

    route users

--- routes/users.js
+++ routes/users.js
@@ -3,3 +3,9 @@ const {
   getCurrentUser,
   updateUser,
 } = require('../controllers/users');
+const { updateUserValidation } = require('../middlewares/validations');
+
+router.get('/users/me', getCurrentUser);
+router.patch('/users/me', updateUserValidation, updateUser);
+
+module.exports = router;
