97b75d74c6de2ae57388731a413bbbf49f2bfa5c
ARTALEAL <artaleal@yandex.ru>
2023-04-12 14:33:04 +0300

    app connect loggers, cors

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const {
 } = require('./utils/constants');
 const router = require('./routes');
 
+const cors = require('./middlewares/cors');
+const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 mongoose.set('strictQuery', true);
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV, {
@@ -21,6 +25,14 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors);
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT);
